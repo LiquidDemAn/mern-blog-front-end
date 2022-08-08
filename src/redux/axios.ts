3ead0efb76782de5ceea47c749b1fbad2d963ca4
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-08-08 16:02:21 +0300

    created middleware for check auth

--- src/redux/axios.ts
+++ src/redux/axios.ts
@@ -3,3 +3,13 @@ import axios from 'axios';
 export const customeAxios = axios.create({
 	baseURL: 'http://localhost:4444',
 });
+
+const token = window.localStorage.getItem('token');
+
+customeAxios.interceptors.request.use((config) => {
+	if (token) {
+		// @ts-ignore
+		config.headers.Authorization = token;
+		return config;
+	}
+});
